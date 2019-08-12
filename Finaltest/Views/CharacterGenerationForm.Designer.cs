fd93bdcc87dcb44cb6e773875ecc0f9f42cace61
milanpreet007 <preetmilan222@gmail.com>
2019-08-12 15:29:55 -0400

    added tab control

--- Finaltest/Views/CharacterGenerationForm.Designer.cs
+++ Finaltest/Views/CharacterGenerationForm.Designer.cs
@@ -28,9 +28,109 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.MainTabControl = new System.Windows.Forms.TabControl();
+            this.IdentityPage = new System.Windows.Forms.TabPage();
+            this.AbilityPage = new System.Windows.Forms.TabPage();
+            this.nextButton = new System.Windows.Forms.Button();
+            this.backButton = new System.Windows.Forms.Button();
+            this.SkillsPage = new System.Windows.Forms.TabPage();
+            this.CharacterSheetPage = new System.Windows.Forms.TabPage();
+            this.MainTabControl.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // MainTabControl
+            // 
+            this.MainTabControl.Controls.Add(this.IdentityPage);
+            this.MainTabControl.Controls.Add(this.AbilityPage);
+            this.MainTabControl.Controls.Add(this.SkillsPage);
+            this.MainTabControl.Controls.Add(this.CharacterSheetPage);
+            this.MainTabControl.Dock = System.Windows.Forms.DockStyle.Top;
+            this.MainTabControl.Location = new System.Drawing.Point(0, 0);
+            this.MainTabControl.Name = "MainTabControl";
+            this.MainTabControl.SelectedIndex = 0;
+            this.MainTabControl.Size = new System.Drawing.Size(784, 429);
+            this.MainTabControl.TabIndex = 0;
+            // 
+            // IdentityPage
+            // 
+            this.IdentityPage.Location = new System.Drawing.Point(4, 33);
+            this.IdentityPage.Name = "IdentityPage";
+            this.IdentityPage.Padding = new System.Windows.Forms.Padding(3);
+            this.IdentityPage.Size = new System.Drawing.Size(776, 392);
+            this.IdentityPage.TabIndex = 0;
+            this.IdentityPage.Text = "Identity";
+            this.IdentityPage.UseVisualStyleBackColor = true;
+            // 
+            // AbilityPage
+            // 
+            this.AbilityPage.Location = new System.Drawing.Point(4, 33);
+            this.AbilityPage.Name = "AbilityPage";
+            this.AbilityPage.Padding = new System.Windows.Forms.Padding(3);
+            this.AbilityPage.Size = new System.Drawing.Size(776, 392);
+            this.AbilityPage.TabIndex = 1;
+            this.AbilityPage.Text = "Abilities";
+            this.AbilityPage.UseVisualStyleBackColor = true;
+            // 
+            // nextButton
+            // 
+            this.nextButton.Location = new System.Drawing.Point(622, 490);
+            this.nextButton.Name = "nextButton";
+            this.nextButton.Size = new System.Drawing.Size(136, 63);
+            this.nextButton.TabIndex = 1;
+            this.nextButton.Text = "next";
+            this.nextButton.UseVisualStyleBackColor = true;
+            this.nextButton.Click += new System.EventHandler(this.nextButton_Click);
+            // 
+            // backButton
+            // 
+            this.backButton.Location = new System.Drawing.Point(29, 490);
+            this.backButton.Name = "backButton";
+            this.backButton.Size = new System.Drawing.Size(136, 63);
+            this.backButton.TabIndex = 1;
+            this.backButton.Text = "back";
+            this.backButton.UseVisualStyleBackColor = true;
+            this.backButton.Click += new System.EventHandler(this.backButton_Click);
+            // 
+            // SkillsPage
+            // 
+            this.SkillsPage.Location = new System.Drawing.Point(4, 33);
+            this.SkillsPage.Name = "SkillsPage";
+            this.SkillsPage.Size = new System.Drawing.Size(776, 392);
+            this.SkillsPage.TabIndex = 2;
+            this.SkillsPage.Text = "Skills";
+            this.SkillsPage.UseVisualStyleBackColor = true;
+            // 
+            // CharacterSheetPage
+            // 
+            this.CharacterSheetPage.Location = new System.Drawing.Point(4, 33);
+            this.CharacterSheetPage.Name = "CharacterSheetPage";
+            this.CharacterSheetPage.Size = new System.Drawing.Size(776, 392);
+            this.CharacterSheetPage.TabIndex = 3;
+            this.CharacterSheetPage.Text = "Character Sheet";
+            this.CharacterSheetPage.UseVisualStyleBackColor = true;
+            // 
+            // CharacterGenerationForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Controls.Add(this.backButton);
+            this.Controls.Add(this.nextButton);
+            this.Controls.Add(this.MainTabControl);
+            this.Name = "CharacterGenerationForm";
+            this.Text = "Character Generator";
+            this.MainTabControl.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TabControl MainTabControl;
+        private System.Windows.Forms.TabPage IdentityPage;
+        private System.Windows.Forms.TabPage AbilityPage;
+        private System.Windows.Forms.Button nextButton;
+        private System.Windows.Forms.Button backButton;
+        private System.Windows.Forms.TabPage SkillsPage;
+        private System.Windows.Forms.TabPage CharacterSheetPage;
     }
 }
--- Finaltest/Views/CharacterGenerationForm.cs
+++ Finaltest/Views/CharacterGenerationForm.cs
@@ -5,6 +5,10 @@ using System.Data;
 using System.Drawing;
 using System.Text;
 using System.Windows.Forms;
+/*STUDENT NAME: MILANPREET SINGH
+ * STUDENT ID:301044674
+ * DESCRIPTION : THIS IS THE MAIN FORM FOR THE APPLICATION 
+  */
 
 namespace Finaltest.Views
 {
@@ -13,6 +17,30 @@ namespace Finaltest.Views
         public CharacterGenerationForm()
         {
             InitializeComponent();
+        }
+        /// <summary>
+        /// This is the event handler for back button
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void backButton_Click(object sender, EventArgs e)
+        {
+            if(MainTabControl.SelectedIndex != 0)
+            {
+                MainTabControl.SelectedIndex--;
+            }
+        }
+        /// <summary>
+        /// This is the event handler for next button
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void nextButton_Click(object sender, EventArgs e)
+        {
+            if(MainTabControl.SelectedIndex < MainTabControl.TabPages.Count -1 )
+            {
+                MainTabControl.SelectedIndex++;
+            }
         }
     }
 }
