1ab0b3620d9f95ebeb6615b3072085c2ac5c0e3f
milanpreet007 <preetmilan222@gmail.com>
2019-08-12 15:40:03 -0400

    added UI for identitPage Tab

--- Finaltest/Views/CharacterGenerationForm.Designer.cs
+++ Finaltest/Views/CharacterGenerationForm.Designer.cs
@@ -31,11 +31,19 @@
             this.MainTabControl = new System.Windows.Forms.TabControl();
             this.IdentityPage = new System.Windows.Forms.TabPage();
             this.AbilityPage = new System.Windows.Forms.TabPage();
-            this.nextButton = new System.Windows.Forms.Button();
-            this.backButton = new System.Windows.Forms.Button();
             this.SkillsPage = new System.Windows.Forms.TabPage();
             this.CharacterSheetPage = new System.Windows.Forms.TabPage();
+            this.nextButton = new System.Windows.Forms.Button();
+            this.backButton = new System.Windows.Forms.Button();
+            this.identitytableLayoutPanel = new System.Windows.Forms.TableLayoutPanel();
+            this.Firstnamelabel = new System.Windows.Forms.Label();
+            this.lastNamelabel = new System.Windows.Forms.Label();
+            this.FirstNameDataLAbel = new System.Windows.Forms.Label();
+            this.LastNamedatalabel = new System.Windows.Forms.Label();
+            this.GenerateNamebutton = new System.Windows.Forms.Button();
             this.MainTabControl.SuspendLayout();
+            this.IdentityPage.SuspendLayout();
+            this.identitytableLayoutPanel.SuspendLayout();
             this.SuspendLayout();
             // 
             // MainTabControl
@@ -53,6 +61,7 @@
             // 
             // IdentityPage
             // 
+            this.IdentityPage.Controls.Add(this.identitytableLayoutPanel);
             this.IdentityPage.Location = new System.Drawing.Point(4, 33);
             this.IdentityPage.Name = "IdentityPage";
             this.IdentityPage.Padding = new System.Windows.Forms.Padding(3);
@@ -70,6 +79,24 @@
             this.AbilityPage.TabIndex = 1;
             this.AbilityPage.Text = "Abilities";
             this.AbilityPage.UseVisualStyleBackColor = true;
+            // 
+            // SkillsPage
+            // 
+            this.SkillsPage.Location = new System.Drawing.Point(4, 33);
+            this.SkillsPage.Name = "SkillsPage";
+            this.SkillsPage.Size = new System.Drawing.Size(776, 392);
+            this.SkillsPage.TabIndex = 2;
+            this.SkillsPage.Text = "Skills";
+            this.SkillsPage.UseVisualStyleBackColor = true;
+            // 
+            // CharacterSheetPage
+            // 
+            this.CharacterSheetPage.Location = new System.Drawing.Point(4, 33);
+            this.CharacterSheetPage.Name = "CharacterSheetPage";
+            this.CharacterSheetPage.Size = new System.Drawing.Size(776, 392);
+            this.CharacterSheetPage.TabIndex = 3;
+            this.CharacterSheetPage.Text = "Character Sheet";
+            this.CharacterSheetPage.UseVisualStyleBackColor = true;
             // 
             // nextButton
             // 
@@ -91,23 +118,93 @@
             this.backButton.UseVisualStyleBackColor = true;
             this.backButton.Click += new System.EventHandler(this.backButton_Click);
             // 
-            // SkillsPage
+            // identitytableLayoutPanel
             // 
-            this.SkillsPage.Location = new System.Drawing.Point(4, 33);
-            this.SkillsPage.Name = "SkillsPage";
-            this.SkillsPage.Size = new System.Drawing.Size(776, 392);
-            this.SkillsPage.TabIndex = 2;
-            this.SkillsPage.Text = "Skills";
-            this.SkillsPage.UseVisualStyleBackColor = true;
+            this.identitytableLayoutPanel.ColumnCount = 4;
+            this.identitytableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.identitytableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.identitytableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.identitytableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.identitytableLayoutPanel.Controls.Add(this.Firstnamelabel, 0, 0);
+            this.identitytableLayoutPanel.Controls.Add(this.GenerateNamebutton, 2, 3);
+            this.identitytableLayoutPanel.Controls.Add(this.lastNamelabel, 0, 1);
+            this.identitytableLayoutPanel.Controls.Add(this.FirstNameDataLAbel, 1, 0);
+            this.identitytableLayoutPanel.Controls.Add(this.LastNamedatalabel, 1, 1);
+            this.identitytableLayoutPanel.Location = new System.Drawing.Point(4, 7);
+            this.identitytableLayoutPanel.Name = "identitytableLayoutPanel";
+            this.identitytableLayoutPanel.RowCount = 4;
+            this.identitytableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.identitytableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.identitytableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.identitytableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.identitytableLayoutPanel.Size = new System.Drawing.Size(769, 309);
+            this.identitytableLayoutPanel.TabIndex = 0;
             // 
-            // CharacterSheetPage
+            // Firstnamelabel
             // 
-            this.CharacterSheetPage.Location = new System.Drawing.Point(4, 33);
-            this.CharacterSheetPage.Name = "CharacterSheetPage";
-            this.CharacterSheetPage.Size = new System.Drawing.Size(776, 392);
-            this.CharacterSheetPage.TabIndex = 3;
-            this.CharacterSheetPage.Text = "Character Sheet";
-            this.CharacterSheetPage.UseVisualStyleBackColor = true;
+            this.Firstnamelabel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Firstnamelabel.Location = new System.Drawing.Point(3, 0);
+            this.Firstnamelabel.Name = "Firstnamelabel";
+            this.Firstnamelabel.Size = new System.Drawing.Size(186, 77);
+            this.Firstnamelabel.TabIndex = 0;
+            this.Firstnamelabel.Text = "First Name";
+            this.Firstnamelabel.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // lastNamelabel
+            // 
+            this.lastNamelabel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lastNamelabel.Location = new System.Drawing.Point(3, 77);
+            this.lastNamelabel.Name = "lastNamelabel";
+            this.lastNamelabel.Size = new System.Drawing.Size(186, 77);
+            this.lastNamelabel.TabIndex = 1;
+            this.lastNamelabel.Text = "Last Name";
+            this.lastNamelabel.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // FirstNameDataLAbel
+            // 
+            this.FirstNameDataLAbel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.FirstNameDataLAbel.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.FirstNameDataLAbel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.identitytableLayoutPanel.SetColumnSpan(this.FirstNameDataLAbel, 3);
+            this.FirstNameDataLAbel.Location = new System.Drawing.Point(195, 0);
+            this.FirstNameDataLAbel.Name = "FirstNameDataLAbel";
+            this.FirstNameDataLAbel.Size = new System.Drawing.Size(571, 77);
+            this.FirstNameDataLAbel.TabIndex = 1;
+            this.FirstNameDataLAbel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // LastNamedatalabel
+            // 
+            this.LastNamedatalabel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.LastNamedatalabel.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.LastNamedatalabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.identitytableLayoutPanel.SetColumnSpan(this.LastNamedatalabel, 3);
+            this.LastNamedatalabel.Location = new System.Drawing.Point(195, 77);
+            this.LastNamedatalabel.Name = "LastNamedatalabel";
+            this.LastNamedatalabel.Size = new System.Drawing.Size(571, 77);
+            this.LastNamedatalabel.TabIndex = 1;
+            this.LastNamedatalabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // GenerateNamebutton
+            // 
+            this.GenerateNamebutton.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.identitytableLayoutPanel.SetColumnSpan(this.GenerateNamebutton, 2);
+            this.GenerateNamebutton.Location = new System.Drawing.Point(387, 234);
+            this.GenerateNamebutton.Name = "GenerateNamebutton";
+            this.GenerateNamebutton.Size = new System.Drawing.Size(379, 72);
+            this.GenerateNamebutton.TabIndex = 1;
+            this.GenerateNamebutton.Text = "Generate Name";
+            this.GenerateNamebutton.UseVisualStyleBackColor = true;
+            this.GenerateNamebutton.Click += new System.EventHandler(this.nextButton_Click);
             // 
             // CharacterGenerationForm
             // 
@@ -119,6 +216,8 @@
             this.Name = "CharacterGenerationForm";
             this.Text = "Character Generator";
             this.MainTabControl.ResumeLayout(false);
+            this.IdentityPage.ResumeLayout(false);
+            this.identitytableLayoutPanel.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -132,5 +231,11 @@
         private System.Windows.Forms.Button backButton;
         private System.Windows.Forms.TabPage SkillsPage;
         private System.Windows.Forms.TabPage CharacterSheetPage;
+        private System.Windows.Forms.TableLayoutPanel identitytableLayoutPanel;
+        private System.Windows.Forms.Label Firstnamelabel;
+        private System.Windows.Forms.Button GenerateNamebutton;
+        private System.Windows.Forms.Label lastNamelabel;
+        private System.Windows.Forms.Label FirstNameDataLAbel;
+        private System.Windows.Forms.Label LastNamedatalabel;
     }
 }
